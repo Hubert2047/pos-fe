b5ea755b51d3d36263a85f247d3e63cb471f30e7
hubert <hubert2047@gmail.com>
2026-03-25 17:29:14 +0800

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
     <>
-      <p className="text-3xl font-bold underline">hello</p>
+       <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
     </>
   )
 }
